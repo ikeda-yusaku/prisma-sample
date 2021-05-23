b5dfc9983fe1973cf9d8d3d64dce4bd7da69bda0
ikeda yusaku <ikeda.yusaku@ikeda-stp.com>
2021-05-24 00:02:04 +0900

    実際に使うための実装検討

--- starter/script.ts
+++ starter/script.ts
@@ -1,6 +1,76 @@
-import { Prisma, PrismaClient } from '@prisma/client'
+import { Prisma, PrismaClient, User } from '@prisma/client'
 
 const prisma = new PrismaClient()
+
+/**
+ * エンティティ
+ * 一応ビジネスロジックを書くために作る
+ * Prismaの生成されたモデルを極力採用したいので
+ * CreateInputとUpdateInputとUserをそれぞれほじするようにする
+ */
+class UserEntity {
+
+  createInput: Prisma.UserCreateInput | undefined
+  updateInput: Prisma.UserUpdateInput | undefined
+  data:User | undefined
+
+  constructor(createInput?:Prisma.UserCreateInput | undefined, updateInput?:Prisma.UserUpdateInput | undefined, data?:User | undefined) {
+    if (createInput != null) {
+      this.createInput = createInput
+    }
+
+    if (updateInput != null) {
+      this.updateInput = updateInput
+    }
+
+    if (data != null) {
+      this.data = data
+    }
+  }
+  validate(): boolean {
+    return true
+  }
+}
+
+/**
+ * UserDataGateway
+ * ユーザーに関する操作のインターフェース
+ */
+interface iUserDataGateWay {
+  AddNewUser(data:UserEntity):Promise<User | undefined>
+}
+
+/**
+ * UserDataAccess
+ * ユーザーに関する操作の実態、Gatewayを継承する
+ * Prismaの処理を呼び出すことにする
+ */
+class UserDataAccess implements iUserDataGateWay {
+  async AddNewUser(user: UserEntity): Promise<User | undefined> {
+    user.validate()
+    if (user.createInput != undefined) {
+      const u = await prisma.user.create({data:user.createInput!})
+      return u
+    }
+    return undefined
+  }
+}
+
+/**
+ * UserUsecaseInteractor
+ */
+class UserUsecaseInteractor {
+  public async handle() {
+    let u : Prisma.UserCreateInput = {
+      email : "nnnnnn"
+    }
+    let us = new UserEntity(u)
+    let uda = new UserDataAccess() //本来はDIを使って処理する流れを掴むために省略
+    await uda.AddNewUser(us)
+  }
+}
+
+
 
 // A `main` function so that you can use async/await
 async function main() {
@@ -22,7 +92,7 @@ async function main() {
    * インサートするためのデータを作る
    * Prismaで勝手に作ってくれたTypeでデータを整理する
    */
-  let u: Prisma.UserCreateInput
+  /*let u: Prisma.UserCreateInput
   u = {
     email:'ddd',
     profile : { //リレーションのレコードはこんな感じでネストで記述できる
@@ -32,11 +102,16 @@ async function main() {
     }
   }
   console.log(u)
+*/
 
   /**
    * createに渡す
    */
-  const us = await prisma.user.create({data:u})
+  //const us = await prisma.user.create({data:u})
+
+
+  var a:UserUsecaseInteractor = new UserUsecaseInteractor()
+  await a.handle()
 
   /**
   { id: 1, email: 'aaa', name: null, posts: [], profile: null },
